bed90d661a2d28dd43a43d9da37a50dc8708c91f
Hong Zhuang <zhuanghong2013@gmail.com>
2017-09-30 03:03:19 +0000

    reverse string

---
+++ Algorithms/TwoPointers/reverseString.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String reverseString(String s) {
+        char[] rev = s.toCharArray();
+        int i = 0;
+        int j = s.length() - 1;
+        while(i < j){
+            char temp = rev[i];
+            rev[i] = rev[j];
+            rev[j] = temp;
+            i++;
+            j--;
+        }
+        return new String(rev);
+    }
+}
