0f4c67dbdcd99f43ed89c39782a2d518aa79dcbe
Hong Zhuang <zhuanghong2013@gmail.com>
2017-09-30 03:42:51 +0000

    single number

--- Algorithms/Math/nimGame.java
+++ Algorithms/Math/nimGame.java
@@ -10,3 +10,8 @@ For example, if there are 4 stones in the heap, then you will never win the game
 no matter 1, 2, or 3 stones you remove, the last stone will always be removed by your friend.
 */
 
+class Solution {
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+}
