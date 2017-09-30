9382f09a6deb4653bf8fcb4eaddf7f57e9eed79d
Hong Zhuang <zhuanghong2013@gmail.com>
2017-09-30 03:20:18 +0000

    two sum

---
+++ DataStructures/HashTable/twoSum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(target - nums[i])){
+                res[0] = i;
+                res[1] = map.get(target - nums[i]);
+            }
+            map.put(nums[i], i);
+        }
+        
+        Arrays.sort(res);
+        return res;
+    }
+}
