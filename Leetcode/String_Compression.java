0bb20861f98a631424e198643c08ec7ee2f1442f
Hong Zhuang <zhuanghong@Hongs-MacBook-Pro.local>
2018-08-20 22:23:53 -0500

    complete string compression

---
+++ Leetcode/String_Compression.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int compress(char[] chars) {
+        int indexAns = 0, index = 0;
+        while(index < chars.length){
+            char currentChar = chars[index];
+            int count = 0;
+            while(index < chars.length && chars[index] == currentChar){
+                index++;
+                count++;
+            }
+            chars[indexAns++] = currentChar;
+            if(count != 1)
+                for(char c : Integer.toString(count).toCharArray()) 
+                    chars[indexAns++] = c;
+        }
+        return indexAns;
+    }
+}
+
+
+
+/*
+    要in place置换，就要用two pointers,一个用来read当前input，一个write结果到当前的input array。
+    维持一个count来记录字符频率.
+*/
